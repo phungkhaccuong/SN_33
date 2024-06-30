89ae82e586effa24f79ef8aa6e315ad281839b65
phungkhaccuong <cuongst93@gmail.com>
2024-06-30 21:51:40 +0700

    save elastic

--- tests/save_elasticsearch.py
+++ tests/save_elasticsearch.py
@@ -23,26 +23,26 @@ def prepare_text_data(conversations):
 
 # Example data
 data = [
-    {
-        'guid': 2293692782,
-        'participants': ['"SPEAKER_00"', '"SPEAKER_02"', '"SPEAKER_01"', '"None"'],
-        'lines': [
-            [0, 'Let s get it on!'],
-            [0,'Welcome back to New Heights, ladies and gentlemen, presented by Wayne Sports and- You re the one that s going to have to bring the energy today.'],
-            [1, 'I want to go to a dark room and not have to interact with anybody for three days.'],
-            [0, 'Welcome back to New Heights, presented by Wayne Sports and Entertainment and- We need more energy.'],
-            [1, 'Come on, you re in the playoffs still.'],
-            [1, 'I need more.'],
-            [1, 'You re going to be good cop, I m bad cop today.'],
-            [0, 'Good cop, good cop.'],
-            [0, 'I m the good cop.'],
-            [0, 'I know, that s what I m saying.'],
-            [0, 'Hey, I just did my first desk pop.'],
-            [0,
-             'Welcome back to New Heights, ladies and gentlemen, presented by Wave Sports and Entertainment and brought to you by the all new Experience Smart Money Debit Card, the debit card that builds credit without the debt.'],
-            [0, 'That s right.']
-        ]
-    }
+    {'guid': 2029571757,
+     'participants': ['"SPEAKER_01"', '"SPEAKER_00"'],
+     'lines': [
+         [0, 'The following is a conversation with Chris Urmson.'],
+         [0, 'He was the CTO of the Google self-driving car team, a key engineer and leader behind the Carnegie Mellon University autonomous vehicle entries in the DARPA Grand Challenges and the winner of the DARPA Urban Challenge.'], [0, 'Today, he s the CEO of Aurora Innovation, an autonomous vehicle software company he started with Sterling Anderson, who was the former director of Tesla Autopilot, and Drew Bagnell, Uber s former autonomy and perception lead.'],
+         [0, 'Chris is one of the top roboticists and autonomous vehicle experts in the world, and a longtime voice of reason in a space that is shrouded in both mystery and hype.'],
+         [0, 'He both acknowledges the incredible challenges involved in solving the problem of autonomous driving and is working hard to solve it.'],
+         [0, 'This is the Artificial Intelligence Podcast.'],
+         [0, 'If you enjoy it, subscribe on YouTube, give it five stars on iTunes, support it on Patreon, or simply connect with me on Twitter at LexFriedman, spelled F-R-I-D-M-A-N.'],
+         [0, 'And now, here s my conversation with Chris Armisen.'],
+         [0, 'You were part of both the DARPA Grand Challenge and the DARPA Urban Challenge teams at CMU with Red Whitaker.'],
+         [0, 'What technical or philosophical things have you learned from these races?'],
+         [1, 'I think the high order bit was that it could be done.'],
+         [1, 'I think that was the thing that was incredible about the first, the Grand Challenges.'],
+         [1, 'that I remember you know I was a grad student at Carnegie Mellon and there we it was kind of this dichotomy of'],
+         [1, 'It seemed really hard, so that would be cool and interesting.'],
+         [1, 'But at the time, we were the only robotics institute around.'],
+         [1, 'And so if we went into it and fell on our faces, that would be embarrassing.']
+            ]
+     }
 ]
 
 if __name__ == '__main__':
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     query = {
         "query": {
             "match": {
-                "lines": "[0, 'I m the good cop.']"
+                "lines": "there we it was kind of this dichotomy of"
             }
         }
     }
