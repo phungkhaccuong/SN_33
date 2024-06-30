72794544d8c3b3acddaa1e172c0298209508dde1
phungkhaccuong <cuongst93@gmail.com>
2024-06-30 23:13:28 +0700

    u

--- tests/save_elasticsearch.py
+++ tests/save_elasticsearch.py
@@ -76,7 +76,7 @@ def del_index(es):
 if __name__ == '__main__':
     elastic = StructuredSearchEngine()
     # Index the data
-    index_data_if_not_exists(elastic.es, data)
+    index_data_if_not_exists(elastic.es, data[0])
 
     # Define a search query for text field mapping
     query = {
